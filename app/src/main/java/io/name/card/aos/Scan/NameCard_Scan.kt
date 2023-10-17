38fb9010e9d768054ae2bc83f0bd3dce1027986e
jiinii1022 <enffl68953@naver.com>
2023-10-18 04:56:36 +0900

    1

--- app/src/main/java/io/name/card/aos/Scan/NameCard_Scan.kt
+++ app/src/main/java/io/name/card/aos/Scan/NameCard_Scan.kt
@@ -14,14 +14,14 @@ import androidx.fragment.app.Fragment
 import io.name.card.aos.R
 
 class nameCard_Scan : Fragment() {
-    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_IMAGE_CAPTURE = 1 // 사진 촬영
+    private val REQUEST_GALLERY_IMAGE = 2 // 갤러리 불러오기
     private lateinit var nameCardScanImage: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Fragment의 레이아웃을 인플레이트하고 반환합니다.
         return inflater.inflate(R.layout.fragment_name_card__scan, container, false)
     }
 
@@ -30,12 +30,19 @@ class nameCard_Scan : Fragment() {
 
         nameCardScanImage =  view.findViewById(R.id.nameCardScan_Image)
         val nameCardScanButton: Button = view.findViewById(R.id.nameCardScan_Button)
+        val nameCardScanImageButton: Button = view.findViewById(R.id.nameCardScan_Image_Button)
 
+        // 사진 촬영 버튼 클릭 리스너
         nameCardScanButton.setOnClickListener {
             dispatchTakePictureIntent()
         }
+        // 갤러리 불러오기 버튼 클릭 리스너
+        nameCardScanImageButton.setOnClickListener {
+            dispatchSelectPictureFromGalleryIntent()
+        }
     }
 
+    // 사진 촬영 화면으로 넘어가기
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
@@ -43,12 +50,23 @@ class nameCard_Scan : Fragment() {
         }
     }
 
+    // 갤러리 앱으로 넘어가기
+    private fun dispatchSelectPictureFromGalleryIntent() {
+        val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(pickPhotoIntent, REQUEST_GALLERY_IMAGE)
+    }
+
+    // 촬영 결과, 갤러리에서 가져온 이미지 가져와서 ImageView에 보이기
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as? Bitmap
             nameCardScanImage.setImageBitmap(imageBitmap)
+        }
+        else if (requestCode == REQUEST_GALLERY_IMAGE && resultCode == RESULT_OK) {
+            val imageUri = data?.data
+            nameCardScanImage.setImageURI(imageUri)
         }
     }
 }
